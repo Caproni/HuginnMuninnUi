2d9804cf10c9ea564135d8d80e7222ad621aae8a
Edmund Bennett <bennettedmund@gmail.com>
2023-04-24 21:24:24 +0100

    Updated to angular 14

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/HuginnMuninnUi'),
+      dir: require('path').join(__dirname, './coverage/bennett-family-site-ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
